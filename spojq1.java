e3d27bf94e87022e2950adac2c2a542c30daaa4e
Aakash Sinha <aakash9868sinha@gmail.com>
2018-08-08 00:52:02 +0530

    Add files via upload

---
+++ spojq1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class scannerTest{
+
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+	while (true){
+		int x = sc.nextInt();
+		if(x== 42)
+			break;
+
+		System.out.println(x);
+		
+	}
+	}
+}
