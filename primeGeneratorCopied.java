f9395cf865f39d85c6d04f9e92e69106b57f1460
Aakash Sinha <aakash9868sinha@gmail.com>
2018-08-10 13:07:35 +0530

    Update and rename Main.java to primeGeneratorCopied.java

--- primeGeneratorCopied.java
+++ primeGeneratorCopied.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 import java.lang.Math.*;
 
-public class Main {
+ class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
